576691ac4a280b7023e10820545d01d690cf9914
s1ckboness <juandavidpenuelaperez24@gmail.com>
2024-11-20 10:34:38 -0500

    ajustes

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('donationForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Evita que se envíe el formulario automáticamente
         
+        const password = document.getElementById('password').value;
+        const passwordError = document.getElementById('passwordError');
+
+        if (!validatePassword(password)) {
+            passwordError.textContent = 'La contraseña debe tener al menos un carácter especial y una letra mayúscula.';
+            passwordError.style.display = 'block';
+            return;
+        } else {
+            passwordError.style.display = 'none';
+        }
+        
         const formData = new FormData(event.target);
         
         const data = {};
@@ -13,13 +24,13 @@ document.addEventListener('DOMContentLoaded', () => {
         donations.push(data);
         localStorage.setItem('donations', JSON.stringify(donations));
 
-       
         const resultElement = document.getElementById('result');
         if (resultElement) {
             resultElement.innerText = '¡Gracias por tu donación!';
         } else {
             console.error('Elemento con id "result" no encontrado');
         }
+
         event.target.reset();
     });
 
@@ -28,3 +39,9 @@ document.addEventListener('DOMContentLoaded', () => {
         alert('Datos de donaciones borrados.');
     });
 });
+
+function validatePassword(password) {
+    const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
+    const uppercasePattern = /[A-Z]/;
+    return specialCharPattern.test(password) && uppercasePattern.test(password);
+}
