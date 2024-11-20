afe517c0bae02fbb82f4c2ae89b978aa5df925f5
s1ckboness <juandavidpenuelaperez24@gmail.com>
2024-11-20 09:56:10 -0500

    cambioFinal

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('donationForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Evita que se envíe el formulario automáticamente
         
-        // Obtener los datos del formulario
         const formData = new FormData(event.target);
         
-        // Convertir los datos del formulario a un objeto para almacenar
         const data = {};
         formData.forEach((value, key) => {
             data[key] = value;
         });
 
-        // Almacenar los datos en el almacenamiento local
         let donations = JSON.parse(localStorage.getItem('donations')) || [];
         donations.push(data);
         localStorage.setItem('donations', JSON.stringify(donations));
 
-        // Verificar y establecer el mensaje de resultado
+       
         const resultElement = document.getElementById('result');
         if (resultElement) {
             resultElement.innerText = '¡Gracias por tu donación!';
         } else {
             console.error('Elemento con id "result" no encontrado');
         }
-
-        // Limpiar el formulario
         event.target.reset();
     });
 
-    // Evento para borrar los datos guardados
     document.getElementById('clearStorage').addEventListener('click', function() {
         localStorage.removeItem('donations');
         alert('Datos de donaciones borrados.');
