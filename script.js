93808a91303877b28f1e4d75a70f24bad675c703
s1ckboness <juandavidpenuelaperez24@gmail.com>
2024-11-20 08:58:08 -0500

    formulario

---
+++ script.js
@@ -0,0 +1,6 @@
+// scripts.js
+document.getElementById('donationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que se envíe el formulario automáticamente
+    alert('¡Gracias por tu donación!');
+    // Aquí puedes añadir el código para procesar y enviar el formulario
+});
