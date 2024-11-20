ab6e869e0ecad3ad30f135754ecf98998f19811c
s1ckboness <juandavidpenuelaperez24@gmail.com>
2024-11-20 09:27:25 -0500

    formulariogood

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// scripts.js
 document.getElementById('donationForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que se envíe el formulario automáticamente
-    alert('¡Gracias por tu donación!');
-    // Aquí puedes añadir el código para procesar y enviar el formulario
+    
+    const formData = new FormData(event.target);
+
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    // Almacenar los datos en el almacenamiento local
+    let donations = JSON.parse(localStorage.getItem('donations')) || [];
+    donations.push(data);
+    localStorage.setItem('donations', JSON.stringify(donations));
+
+    document.getElementById('result').innerHTML = '¡Gracias por tu donación!';
+    
+    event.target.reset();
 });
